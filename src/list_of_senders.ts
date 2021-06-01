14b380eed468fc1ff4fd9f9064d7b006c5590c55
Timur Bondarenko <timur.bondarenko@sofoil.com>
2021-06-01 21:56:18 +0300

    add uuid to adding new item

---
+++ src/list_of_senders.ts
@@ -0,0 +1,56 @@
+import { TargetEnum } from "./models";
+import { v4 as uuid4 } from "uuid";
+
+export type SenderInputModel = {
+  target: string;
+  subject: string;
+  date: string;
+};
+
+export type SenderType = {
+  id: string;
+  target: string;
+  count_of_days: number;
+  last_send_date: string;
+  event_date: string;
+  subject: string;
+};
+
+export class ListOfSenders {
+  listOfSenders: SenderType[];
+  constructor() {
+    this.listOfSenders = [
+      {
+        id: "123123123",
+        target: TargetEnum.telegram,
+        count_of_days: 42,
+        last_send_date: "last_send_date",
+        event_date: "2021-06-01",
+        subject: "1231234124",
+      },
+      {
+        id: "123123123",
+        target: TargetEnum.email,
+        count_of_days: 42,
+        last_send_date: "last_send_date",
+        event_date: "2021-06-01",
+        subject: "1231234124",
+      },
+    ];
+  }
+
+  getList() {
+    return this.listOfSenders;
+  }
+
+  addItem(newSender: SenderInputModel) {
+    this.listOfSenders.push({
+      id: uuid4(),
+      target: TargetEnum[newSender.target],
+      count_of_days: null,
+      last_send_date: "",
+      event_date: newSender.date,
+      subject: newSender.subject,
+    });
+  }
+}
--- src/rest_api.ts
+++ src/rest_api.ts
@@ -1,59 +1,5 @@
 import express from "express";
-import { TargetEnum } from "./models";
-
-type SenderInputModel = {
-  target: string;
-  subject: string;
-  date: string;
-};
-
-type SenderType = {
-  id: string;
-  target: string;
-  count_of_days: string;
-  last_send_date: string;
-  event_date: string;
-  subject: string;
-};
-
-class ListOfSenders {
-  listOfSenders: SenderType[];
-  constructor() {
-    this.listOfSenders = [
-      {
-        id: "123123123",
-        target: TargetEnum.telegram,
-        count_of_days: "42",
-        last_send_date: "last_send_date",
-        event_date: "2021-06-01",
-        subject: "1231234124",
-      },
-      {
-        id: "123123123",
-        target: TargetEnum.email,
-        count_of_days: "42",
-        last_send_date: "last_send_date",
-        event_date: "2021-06-01",
-        subject: "1231234124",
-      },
-    ];
-  }
-
-  getList() {
-    return this.listOfSenders;
-  }
-
-  addItem(newSender: SenderInputModel) {
-    this.listOfSenders.push({
-      id: "123123123",
-      target: TargetEnum[newSender.target],
-      count_of_days: "42",
-      last_send_date: "last_send_date",
-      event_date: newSender.date,
-      subject: newSender.subject,
-    });
-  }
-}
+import { ListOfSenders, SenderInputModel } from "./list_of_senders";
 
 const listOfSenders = new ListOfSenders();
 
@@ -63,7 +9,6 @@ export const rest_api_fill = (app: express.Express) => {
   });
 
   app.post("/list-of-senders/add/", (req: { body: SenderInputModel }, res) => {
-    console.log(req.body);
     listOfSenders.addItem(req.body);
   });
 };
