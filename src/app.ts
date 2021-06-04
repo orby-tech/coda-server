cb595430f36c8971d485378074f4a4c1bd7b5cf5
orby-tech <tibode495@gmail.com>
2021-06-04 19:50:03 +0300

    add eslint

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,15 @@
-import express from "express";
-import cors from "cors";
-import { rest_api_fill } from "./rest_api";
-const app = express();
-const port = 4000;
+import express from 'express'
+import cors from 'cors'
+import { restApiFill } from './rest_api'
+const app = express()
+const port = 4000
 
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-rest_api_fill(app);
+restApiFill(app)
 
 app.listen(port, () => {
-  return console.log(`server is listening on ${port}`);
-});
+  return console.log(`server is listening on ${port}`)
+})
--- src/rest_api.ts
+++ src/rest_api.ts
@@ -1,18 +1,18 @@
-import express from "express";
-import { ListOfSenders, SenderInputModel } from "./list_of_senders";
+import express from 'express'
+import { ListOfSenders, SenderInputModel } from './list_of_senders'
 
-const listOfSenders = new ListOfSenders();
+const listOfSenders = new ListOfSenders()
 
-export const rest_api_fill = (app: express.Express) => {
-  app.get("/list-of-senders/", (req, res) => {
-    res.send(listOfSenders.getList());
-  });
+export const restApiFill = (app: express.Express) => {
+  app.get('/list-of-senders/', (req, res) => {
+    res.send(listOfSenders.getList())
+  })
 
-  app.post("/list-of-senders/add/", (req: { body: SenderInputModel }, res) => {
-    listOfSenders.addItem(req.body);
-  });
+  app.post('/list-of-senders/add/', (req: { body: SenderInputModel }, res) => {
+    listOfSenders.addItem(req.body)
+  })
 
-  app.post("/list-of-senders/delete/", (req: { body: { id: string } }, res) => {
-    listOfSenders.deleteItem(req.body.id);
-  });
-};
+  app.post('/list-of-senders/delete/', (req: { body: { id: string } }, res) => {
+    listOfSenders.deleteItem(req.body.id)
+  })
+}
